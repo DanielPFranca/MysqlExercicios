c6f5d79132d5045438874ee4d08470758d192e1f
DanielPFranca <123706802+DanielPFranca@users.noreply.github.com>
2023-03-30 14:57:00 -0300

    Update Netflix2.py

--- netflix2.sql
+++ netflix2.sql
@@ -1,83 +1,115 @@
-create database netflix2;
-use netflix2;
+create database notafiscal2;
+use notafiscal2;
 
-create table category(
+create table resellers(
 	id bigint not null auto_increment,
-	categoryname varchar(60),
-	primary key(id)
-);
-
-
-create table film(
-	id bigint not null auto_increment,
-	title varchar(100) not null,
-	imageURL varchar(200) not null,
-	exhibitions numeric(10) not null,
-    namecateFK bigint not null,
-	synopsis varchar(100) not null,
-    creationdate date not null,
-    foreign key(namecateFK) references category(id),
+    name varchar(40) not null,
+    email varchar(50) not null,
+    cnpj varchar(14) not null,
     primary key(id)
 );
 
-create table plan(
+
+create table invoice(
 	id bigint not null auto_increment,
-    plan enum("Basic", "Premium", "Gold") not null,
-    price decimal(10,2) not null,
+    invoiceKey varchar(50) not null,
+    dtIssue date,
+    totalInvoice numeric(10) not null,
     primary key(id)
 );
 
-alter table plan modify price numeric(10,2) not null;
 
-
-create table users(
+create table invoicedetails(
 	id bigint not null auto_increment,
-    username varchar(40) not null,
-    email varchar(100) not null,
-    planFK bigint not null,
+    invoiceFK bigint not null,
+    productFK bigint not null,
+    quantity int not null,
+    unitValue decimal(10,2),
     primary key(id),
-    foreign key(planFK) references plan(id)
+    foreign key(invoiceFK) references invoice(id),
+    foreign key(productFK) references products(id)
 );
 
 
-create table payments(
+
+create table products(
 	id bigint not null auto_increment,
-    userFK bigint not null,
-    datepayment date not null,
-    amountFK bigint not null,
-    foreign key(userFK) references users(id),
-    foreign key(amountFK) references plan(id),
+	productCode numeric(10) not null,
+    category varchar(30) not null,
+    description varchar(50) not null,
+    status enum("Ativo", "Inativo") not null,
+    regisDate date not null,
+    primary key(id)
+);
+
+alter table products modify regisDate datetime not null default now();
+alter table products modify productCode numeric(10) not null;
+
+create table custumerreseller(
+	id bigint not null auto_increment,
+    customerFK bigint not null,
+    resellerFK bigint not null,
+    productFK bigint not null,
+    foreign key(customerFK) references customers(id),
+    foreign key(resellerFK) references resellers(id),
+    foreign key(productFK) references products(id),
     primary key(id)
 );
 
 
-insert into category(categoryname) values ("Ação"), ("Aventura"), ("Ficção Científica"), ("Terror");
 
-insert into plan(plan, price) values ("Basic", "23.90"), ("Premium", "32.90"), ("Gold", "40.90");
-
-insert into users(username, email, planFK) values ("Heittor", "heittor@gmail", 2), ("Jão", "jãocraftgames@gmail.com", 1), ("Valentina", "vava2007@gmail.com", 3);
-
-insert into film(title, imageURL, exhibitions, namecateFK, synopsis, creationdate) values ("007", "www.007image.com", 5360, 1, "Muito brabo", "2021-02-25"), 
-("A fuga das galinhas", "www.pintadinhaimage.com", 1302, 2, "O ataque ao galinheiro", "2013-05-13"), 
-("O jogador N1", "www.n1image.com", 4782, 3, "Loucura na realidade virtual", "2018-10-25"), ("Chucky", "www.imageterror.com", 3024, 4, "A morte do carinha", "2010-05-09");
-
-insert into payments(userFK, datepayment, amountFK) values (3, "2023-02-14", 3), (2, "2022-12-18", 1), (1, "2021-06-14", 2);
-
-select p.amountFK, pn.plan, pn.price from payments p
-inner join plan pn on pn.id = p.amountFK;
-
-select title, exhibitions from film 
-where exhibitions = (select max(exhibitions) from film) or exhibitions = (select min(exhibitions) from film);
-
-
-select title ,max(exhibitions), min(exhibitions)from film 
-group by title;
-
-select * from film order by 4;
+create table customers(
+	id bigint not null auto_increment,
+    name varchar(50) not null,
+    email varchar(50) not null,
+    cnpj varchar(14) not null,
+    primary key(id)
+);
 
 
 
 
+insert into resellers(name, email, cnpj) values ("reseller A", "revenda1@gmail.com", "00006371823495"), 
+("reseller B", "revenda2@gmail.com","00003892031234"),("reseller C", "revenda3@gmail.com", "00003947856313");
+select * from customers;
+
+insert into customers (name, email, cnpj) values ("Cliente A","CLA@gmail.com", "00003726178923"),
+("Cliente B", "CLB@gmail.com", "00009873561298"), ("ClienteC", "CLC@gmail.com", "00004728934678");
+
+insert into invoice (invoiceKey, dtIssue, totalInvoice) values ("3728910412", "2020-11-23", "1000"), ("1390458235","2021-02-21","1500"),
+("1230493402", "2022-10-16", "250");
+
+insert into products(productCode, category, description, status, regisDate) values ("319230123", "Eletrônicos", "Celular", "Ativo", "2021-05-12"),
+("3123021330", "Móveis", "Guarda-Roupa", "Inativo", "2022-01-10"), ("3910249520", "Eletrodomésticos", "Ventilador", "Ativo", "2023-03-30");
+
+insert into custumerreseller(customerFK, resellerFK, productFK) values ("1", "2", "3"), ("2", "1", "3"), ("2", "1", "1"), 
+("3", "3", "4"), ("1", "1", "3");
+
+insert into invoicedetails (invoiceFK, productFK, quantity, unitValue) values ("1", "1", "54", "1000.90"), ("2", "3", "120", "1500.90"), ("3", "4", "950", "250.00");
+
+select * from custumerreseller;
+
+#####################################
+
+select p.description, c.productFK from products p
+inner join custumerreseller c on p.id = c.productFK 
+group by productFK having count(*) > 2; 
+
+#####################################
+
+select * from customers c
+inner join custumerreseller cr on c.id = cr.productFK;
+
+#####################################
+
+
+
+
+######################################
+#where productFK = (select max(productFK)from custumerreseller);
+
+delete from products
+where id = 2;
 
 
 
